cb2135f2ec76458cbf55241c1d84d3a03c07a83a
Hieu Pham <hieupham.code@gmail.com>
2024-03-26 15:48:12 +0700

    dark mode completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,13 +8,16 @@ import 'react-toastify/dist/ReactToastify.css'
 import { store, persistor } from './redux/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import ThemeProvider from './components/ThemeProvider.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
         <ToastContainer theme="colored"/>
       </Provider>
     </PersistGate>
